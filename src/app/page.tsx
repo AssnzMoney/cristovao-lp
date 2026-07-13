50a7dd574394ed011ace872a3efa07063e38023c
Assnzcreator <alisongeneral25@gmail.com>
2026-07-13 14:32:56 -0300

    style: remove logo no topo da versao mobile e tira area de depoimentos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/PortalBrilho/Hero";
 import PainCycle from "@/components/PortalBrilho/PainCycle";
 import Deliverables from "@/components/PortalBrilho/Deliverables";
-import SocialProof from "@/components/PortalBrilho/SocialProof";
 import AboutMaster from "@/components/PortalBrilho/AboutMaster";
 import Pricing from "@/components/PortalBrilho/Pricing";
 import Footer from "@/components/PortalBrilho/Footer";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <SocialProof />
       <PainCycle />
       <Deliverables />
       <AboutMaster />
